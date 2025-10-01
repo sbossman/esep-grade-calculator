7156ab00de6972c521bce98a4e66716665d989c0
Summer Bossman <summer.bossman@gmail.com>
2025-10-01 16:12:35 -0400

    Added test cases for 100% code coverage

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,38 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 71, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 75, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 61, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 65, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -49,3 +81,49 @@ func TestGetGradeF(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+func TestCalculateNumericalGrade(t *testing.T) {
+	expected_value := 100
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("exam 1", 100, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
+
+	actual_value := gradeCalculator.calculateNumericalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected calculateNumericalGrade to return '%d'; got '%d' instead", expected_value, actual_value)
+	}
+}
+
+func TestComputeAverage(t *testing.T) {
+	expected_value := 50
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 40, Assignment)
+	gradeCalculator.AddGrade("resume assignment", 50, Assignment)
+	gradeCalculator.AddGrade("unit testing assignment", 60, Assignment)
+
+	actual_value := computeAverage(gradeCalculator.assignments)
+
+	if expected_value != actual_value {
+		t.Errorf("Expected computeAverage to return '%d'; got '%d' instead", expected_value, actual_value)
+	}
+}
+
+func TestString(t *testing.T) {
+	expected_value := "assignment"
+
+	gradeType := Assignment
+
+	actual_value := gradeType.String()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected calculateNumericalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+
